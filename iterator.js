42b95a03c8d68bca8929f677d1567c76255fcc61
AsdoshDev <karthikdev27@gmail.com>
2021-01-25 13:14:28 +0530

    Create iterator.js

---
+++ iterator.js
@@ -0,0 +1,20 @@
+ function Iterator(items){
+   this.items = items;
+   this.index = 0;
+ }
+
+Iterator.prototype = {
+  hasNext : function(){
+    return this.index < this.items.length
+  },
+  
+  next : function(){
+  return this.items[this.index++]
+  }
+}
+
+
+let arr = [1,2,3,4,5];
+
+
+let itr = new Iterator(arr);
